789d3a69725f52512a92597c53d6539da1d94b12
Sol-ml <sol.gladinier25@gmail.com>
2022-08-17 21:41:21 -0300

    fix title

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { createContext, useState } from "react";
+import ReactSwitch from "react-switch";
+import Calculadora from "./components/Calculadora";
+
+export const ThemeContext = createContext(null);
+/* import ModeDark from "./components/ModeDark"; */
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App flex-col " id={theme}>
+        <div className="w-full h-12 flex justify-center aling-center">
+          <h1 className="text-5xl text-slate-800"> Calculadora React</h1>
+        </div>
+        <Calculadora />
+        <div className="switch">
+          <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+        </div>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
